e113988200919c6d0b71f5c7cf196e3de5088d90
Manoj Baishya <52435683+manoj155@users.noreply.github.com>
2022-07-23 14:20:02 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,25 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    while wall_on_right():
+        move()
+    while not wall_on_right():
+        turn_right()
+        move()
+        turn_right()
+        while front_is_clear():
+            move()
+        turn_left()
+    
+        
+
+while not at_goal():
+    if not wall_in_front():
+        move()
+    else:
+        jump()
